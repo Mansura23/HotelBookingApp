9b993a5eb9e53197cd41c60b4074ce8d8c49a09e
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-04-18 17:14:51 +0400

    Hotel and Room entities created

--- src/main/java/org/ironhack/hotelbookingapp/entity/Hotel.java
+++ src/main/java/org/ironhack/hotelbookingapp/entity/Hotel.java
@@ -1,4 +1,23 @@
 package org.ironhack.hotelbookingapp.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Hotel {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String country;
+
+    private String address;
+
+    private double rating;
+
+    @OneToMany(mappedBy="hotel")
+    private List<Room> rooms;
 }
