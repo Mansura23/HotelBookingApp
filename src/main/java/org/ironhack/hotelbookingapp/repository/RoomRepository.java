bd4e4af3d81899fb05750ac0cc813a7632c45c33
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-04-20 17:50:18 +0400

    Room Repository created

--- src/main/java/org/ironhack/hotelbookingapp/repository/RoomRepository.java
+++ src/main/java/org/ironhack/hotelbookingapp/repository/RoomRepository.java
@@ -1,4 +1,27 @@
 package org.ironhack.hotelbookingapp.repository;
 
-public interface RoomRepository {
+import org.ironhack.hotelbookingapp.entity.Room;
+import org.ironhack.hotelbookingapp.enums.RoomType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    Optional<Room> findByRoomNumber(String roomNumber);
+
+    List<Room> findByPricePerNightBetween(double min,double max);
+
+    List<Room> findByType(RoomType roomType);
+
+    List<Room> findByHotelId(Long hotelId);
+
+    boolean existsByAvailableAndType(boolean available, RoomType roomType);
+
+    List<Room> findByAvailableTrue();
+
+    List<Room> findByHotelIdAndAvailableTrue(Long hotelId);
+
+    List<Room> findByHotelIdAndTypeAndAvailableTrue(Long hotelId,RoomType roomType);
 }
