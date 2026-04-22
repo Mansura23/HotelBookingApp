2385c389f060ddccacb04d5d5cd92b1d2aaad578
Mansura <badalovamansura40@gmail.com>
2026-04-22 14:15:36 +0400

    fixing HotelRepository

--- src/main/java/org/ironhack/hotelbookingapp/repository/HotelRepository.java
+++ src/main/java/org/ironhack/hotelbookingapp/repository/HotelRepository.java
@@ -1,4 +1,9 @@
 package org.ironhack.hotelbookingapp.repository;
 
-public interface HotelRepository {
+import org.ironhack.hotelbookingapp.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HotelRepository  extends JpaRepository<Hotel,Long> {
 }
