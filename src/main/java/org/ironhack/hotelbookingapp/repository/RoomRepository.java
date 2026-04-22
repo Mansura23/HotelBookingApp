a767ec3d12ad33470ea435624026676420a58ac3
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-04-22 16:08:37 +0400

    RoomController created

--- src/main/java/org/ironhack/hotelbookingapp/repository/RoomRepository.java
+++ src/main/java/org/ironhack/hotelbookingapp/repository/RoomRepository.java
@@ -2,8 +2,11 @@ package org.ironhack.hotelbookingapp.repository;
 
 import org.ironhack.hotelbookingapp.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
     List<Room> findByHotelId(Long hotelId);
