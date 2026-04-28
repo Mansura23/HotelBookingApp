3490bdc6796396faff6c80522e4f606585513d99
Mansura <badalovamansura40@gmail.com>
2026-04-28 17:53:46 +0400

    deleting unused part

--- src/main/java/org/ironhack/hotelbookingapp/dto/request/AdminUpdateRequestDto.java
+++ src/main/java/org/ironhack/hotelbookingapp/dto/request/AdminUpdateRequestDto.java
@@ -1,8 +1,5 @@
 package org.ironhack.hotelbookingapp.dto.request;
-
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.ironhack.hotelbookingapp.enums.Role;
