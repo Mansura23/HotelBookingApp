520173097adef0bada480f060e7b5779da077ad4
Mansura <badalovamansura40@gmail.com>
2026-04-28 17:31:47 +0400

    adding UserNotFoundException

---
+++ src/main/java/org/ironhack/hotelbookingapp/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package org.ironhack.hotelbookingapp.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
