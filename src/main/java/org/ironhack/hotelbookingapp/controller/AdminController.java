09d6b6a475ef6861ada7a764f0e05653699fe4ca
Mansura <badalovamansura40@gmail.com>
2026-04-28 17:33:20 +0400

    adding AdminController

---
+++ src/main/java/org/ironhack/hotelbookingapp/controller/AdminController.java
@@ -0,0 +1,32 @@
+package org.ironhack.hotelbookingapp.controller;
+
+import org.ironhack.hotelbookingapp.dto.request.AdminUpdateRequestDto;
+import org.ironhack.hotelbookingapp.dto.response.UserResponseDto;
+import org.ironhack.hotelbookingapp.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // admin
+    @PutMapping("/{id}")
+    public UserResponseDto updateByAdmin(@PathVariable Long id,
+                                         @RequestBody AdminUpdateRequestDto dto) {
+        return userService.updateByAdmin(id, dto);
+    }
+
+
+    @DeleteMapping("/{id}")
+    UserResponseDto deleteUser(@PathVariable Long id) {
+        return userService.softDeleteUserById(id);
+    }
+
+
+}
