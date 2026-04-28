e5f0a54fc152e003d4338202a4cbb75725792f9c
Mansura <badalovamansura40@gmail.com>
2026-04-28 17:32:01 +0400

    adding UserMapper

---
+++ src/main/java/org/ironhack/hotelbookingapp/mapper/UserMapper.java
@@ -0,0 +1,42 @@
+package org.ironhack.hotelbookingapp.mapper;
+
+import org.ironhack.hotelbookingapp.dto.request.AdminUpdateRequestDto;
+import org.ironhack.hotelbookingapp.dto.request.UpdateUserRequestDto;
+import org.ironhack.hotelbookingapp.dto.request.UserRequestDto;
+import org.ironhack.hotelbookingapp.dto.response.UserResponseDto;
+import org.ironhack.hotelbookingapp.entity.User;
+import org.ironhack.hotelbookingapp.enums.Role;
+import org.ironhack.hotelbookingapp.enums.Status;
+
+public class UserMapper {
+
+    public static User toEntity(UserRequestDto userRequestDto) {
+        User user = new User();
+        user.setFirstName(userRequestDto.getFirstName());
+        user.setLastName(userRequestDto.getLastName());
+        user.setEmail(userRequestDto.getEmail());
+        user.setRole(Role.USER);
+        user.setBalance(userRequestDto.getBalance());
+        user.setNumber(userRequestDto.getNumber());
+        user.setPassword(userRequestDto.getPassword());
+        user.setStatus(Status.ACTIVE);
+
+        return user;
+    }
+
+    public static UserResponseDto userToUserResponseDto(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(user.getId());
+        userResponseDto.setFirstName(user.getFirstName());
+        userResponseDto.setLastName(user.getLastName());
+        userResponseDto.setEmail(user.getEmail());
+        userResponseDto.setRole(user.getRole());
+        userResponseDto.setNumber(user.getNumber());
+        userResponseDto.setStatus(user.getStatus());
+        userResponseDto.setBalance(user.getBalance());
+
+
+        return userResponseDto;
+    }
+
+}
