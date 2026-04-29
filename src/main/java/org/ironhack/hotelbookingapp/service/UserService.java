f3d11992e2c1904802e718061a9a7c90e1a9489a
Mansura <badalovamansura40@gmail.com>
2026-04-29 22:48:01 +0400

    changing UserService about security

--- src/main/java/org/ironhack/hotelbookingapp/service/UserService.java
+++ src/main/java/org/ironhack/hotelbookingapp/service/UserService.java
@@ -2,36 +2,81 @@ package org.ironhack.hotelbookingapp.service;
 
 import jakarta.transaction.Transactional;
 import org.ironhack.hotelbookingapp.dto.request.AdminUpdateRequestDto;
+import org.ironhack.hotelbookingapp.dto.request.LoginRequest;
 import org.ironhack.hotelbookingapp.dto.request.UpdateUserRequestDto;
 import org.ironhack.hotelbookingapp.dto.request.UserRequestDto;
 import org.ironhack.hotelbookingapp.dto.response.UserResponseDto;
 import org.ironhack.hotelbookingapp.entity.User;
 import org.ironhack.hotelbookingapp.enums.Status;
+import org.ironhack.hotelbookingapp.exception.UserExistsException;
+import org.ironhack.hotelbookingapp.exception.UserNotActiveException;
 import org.ironhack.hotelbookingapp.exception.UserNotFoundException;
 import org.ironhack.hotelbookingapp.mapper.UserMapper;
 import org.ironhack.hotelbookingapp.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private  final AuthenticationManager authenticationManager;
+    private final JWTService jwtService;
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, AuthenticationManager authenticationManager, JWTService jwtService) {
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
     public UserResponseDto registerUser(UserRequestDto userRequestDto) {
         if(userRepository.findByEmail(userRequestDto.getEmail())!=null) {
-            throw new RuntimeException("Email already exists");
+            throw new UserExistsException("User with email already exists");
         }
         User user = UserMapper.toEntity(userRequestDto);
+        user.setPassword(Objects.requireNonNull(encoder.encode(user.getPassword())));
         userRepository.save(user);
         return UserMapper.userToUserResponseDto(user);
     }
 
+//    public String loginUser(UserRequestDto userRequestDto) {
+//        User user=UserMapper.toEntity(userRequestDto);
+//        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+//        if (authentication.isAuthenticated()) {
+//            User dbUser = userRepository.findByEmail(user.getEmail());
+//            return jwtService.generateToken(dbUser.getEmail());
+//        } else {
+//            return "fail";
+//        }
+//
+//    }
+
+
 //    public UserResponseDto loginUser(UserRequestDto userRequestDto) {}
+
+
+    public String loginUser(LoginRequest request) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(request.getEmail());
+        }
+
+        throw new RuntimeException("Invalid credentials");
+    }
     @Transactional
     public  User findById(Long id) {
        return userRepository.findById(id)
@@ -39,23 +84,24 @@ public class UserService {
     }
 
     // USER
-    @Transactional
-    public UserResponseDto updateMe(Long userId,
-                                    UpdateUserRequestDto dto) {
-        User user=findById(userId);
-        if(dto.getFirstName() != null) {
-            user.setFirstName(dto.getFirstName());
+    public UserResponseDto updateMe(UpdateUserRequestDto dto, String authHeader) {
+
+        String token = authHeader.substring(7);
+        String email = jwtService.extractUserName(token);
+        User user = userRepository.findByEmail(email);
+        if (user.getStatus()!=Status.ACTIVE) {
+            throw new UserNotActiveException("User is not active");
         }
-        if(dto.getLastName() != null) {
-            user.setLastName(dto.getLastName());
-        }
-        if(dto.getNumber() != null) {
-            user.setNumber(dto.getNumber());
-        }
+        if(dto.getFirstName() != null) user.setFirstName(dto.getFirstName());
+        if(dto.getLastName() != null) user.setLastName(dto.getLastName());
+        if(dto.getNumber() != null) user.setNumber(dto.getNumber());
+
         if(dto.getPassword() != null) {
-            user.setPassword(dto.getPassword());
+            user.setPassword(encoder.encode(dto.getPassword()));
         }
+
         userRepository.save(user);
+
         return UserMapper.userToUserResponseDto(user);
     }
     // ADMIN
@@ -76,7 +122,7 @@ public class UserService {
         }
 
         if(dto.getPassword() != null) {
-            user.setPassword(dto.getPassword()); //encode
+            user.setPassword(encoder.encode(dto.getPassword()));
         }
 
         if(dto.getRole() != null) {
@@ -109,9 +155,19 @@ public class UserService {
             throw new RuntimeException("Amount must be positive");
         }
         User user = findById(userId);
+        if(user.getStatus()!=Status.ACTIVE) {
+            throw new UserNotActiveException("User is not active");
+        }
         user.setBalance(user.getBalance().add(amount));
         userRepository.save(user);
         return UserMapper.userToUserResponseDto(user);
+    }
+
+    public List<UserResponseDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserMapper::userToUserResponseDto)
+                .toList();
     }
 
 
