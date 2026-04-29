7de56f28fc9b45d825d70cf75cb7df32eba2e87d
Mansura <badalovamansura40@gmail.com>
2026-04-29 22:37:12 +0400

    adding preaAuthorize to RoomController

--- src/main/java/org/ironhack/hotelbookingapp/controller/RoomController.java
+++ src/main/java/org/ironhack/hotelbookingapp/controller/RoomController.java
@@ -7,12 +7,14 @@ import org.ironhack.hotelbookingapp.dto.response.RoomResponseDto;
 import org.ironhack.hotelbookingapp.service.RoomService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/rooms")
+@RequestMapping("admin/rooms")
+@PreAuthorize("hasRole('ADMIN')")
 public class RoomController {
 
     private final RoomService roomService;
