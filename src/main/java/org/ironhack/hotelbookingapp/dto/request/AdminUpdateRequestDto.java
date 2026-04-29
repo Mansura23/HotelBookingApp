03c9c9850f9c437c43f7ec218ecb0c4d628f41ac
Mansura <badalovamansura40@gmail.com>
2026-04-29 22:47:19 +0400

    adding AdminUpdateRequestDto

--- src/main/java/org/ironhack/hotelbookingapp/dto/request/AdminUpdateRequestDto.java
+++ src/main/java/org/ironhack/hotelbookingapp/dto/request/AdminUpdateRequestDto.java
@@ -9,26 +9,11 @@ import java.math.BigDecimal;
 
 @Data
 public class AdminUpdateRequestDto {
-
-    @NotBlank(message = "FirstName is required.")
     private String firstName;
-
-    @NotBlank(message = "LastName is required.")
     private String lastName;
-
-    @NotBlank(message = "Number is required")
     private String number;
-
-    @Size(min = 4, max = 20, message = "Password must be between 4 and 20 characters.")
-    @NotBlank(message = "Password is required")
     private String password;
-
-    @NotBlank(message = "Qualify role.")
     private Role role;
-
-    @NotBlank(message = "Status is required.")
     private Status status;
-
-    @NotBlank
     private BigDecimal balance;
 }
