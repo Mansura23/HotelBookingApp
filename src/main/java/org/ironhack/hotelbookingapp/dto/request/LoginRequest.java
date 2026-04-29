e7e723c864c6f4b85f5c156bb967f4fc869fb3b3
Mansura <badalovamansura40@gmail.com>
2026-04-29 22:53:48 +0400

    adding LoginRequest

---
+++ src/main/java/org/ironhack/hotelbookingapp/dto/request/LoginRequest.java
@@ -0,0 +1,16 @@
+package org.ironhack.hotelbookingapp.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @Email(message = "Email format must be correct.")
+    private String email;
+
+    @Size(min = 4, max = 20, message = "Password must be between 4 and 20 characters.")
+    @NotBlank(message = "Password is required")
+    private String password;
+}
