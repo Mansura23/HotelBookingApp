d0477b0d17ebd762d44eea2eb7da21d49c70d0c6
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-05-01 14:03:12 +0400

    Global exception handler done

--- src/main/java/org/ironhack/hotelbookingapp/exception/GlobalExceptionHandler.java
+++ src/main/java/org/ironhack/hotelbookingapp/exception/GlobalExceptionHandler.java
@@ -60,60 +60,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
-    @ExceptionHandler(DuplicateResourceException.class)
-    public ResponseEntity<ErrorResponseDto> handleDuplicate(DuplicateResourceException ex, HttpServletRequest request){
-        ErrorResponseDto response=new ErrorResponseDto(
-                HttpStatus.CONFLICT.value(),
-                "Conflict",
-                ex.getMessage(),
-                request.getRequestURI(),
-                LocalDateTime.now()
-        );
-
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
-    }
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorResponseDto> handleNotUserNotFound(UserNotFoundException ex, HttpServletRequest request){
-        ErrorResponseDto response=new ErrorResponseDto(
-                HttpStatus.NOT_FOUND.value(),
-                "User not found",
-                ex.getMessage(),
-                request.getRequestURI(),
-                LocalDateTime.now()
-        );
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-    }
-
-    @ExceptionHandler(UserNotActiveException.class)
-    public ResponseEntity<ErrorResponseDto> handleUserNotActive(UserNotActiveException ex, HttpServletRequest request){
-        ErrorResponseDto response=new ErrorResponseDto(
-                HttpStatus.BAD_REQUEST.value(),
-                "User not active",
-                ex.getMessage(),
-                request.getRequestURI(),
-                LocalDateTime.now()
-        );
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    @ExceptionHandler(UserExistsException.class)
-    public ResponseEntity<ErrorResponseDto> handleUserExists(UserExistsException ex, HttpServletRequest request){
-        ErrorResponseDto response=new ErrorResponseDto(
-                HttpStatus.BAD_REQUEST.value(),
-                "User already exists",
-                ex.getMessage(),
-                request.getRequestURI(),
-                LocalDateTime.now()
-        );
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-
-
-
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGeneral(Exception ex,HttpServletRequest request){
@@ -126,5 +72,122 @@ public class GlobalExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+    @ExceptionHandler(CancelledCanNotUpDateException.class)
+    public ResponseEntity<ErrorResponseDto> handleCancelledCanNotUpDate(CancelledCanNotUpDateException ex,HttpServletRequest request){
+        ErrorResponseDto response=new ErrorResponseDto(
+                HttpStatus.CONFLICT.value(),
+                ex.getMessage(),
+                "Cancelled resource cannot be updated.",
+                request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    @ExceptionHandler(CanNotChangeDurationException.class)
+    public ResponseEntity<ErrorResponseDto> handleCanNotChangeDuration(CanNotChangeDurationException ex,HttpServletRequest request){
+        ErrorResponseDto response=new ErrorResponseDto(
+                HttpStatus.CONFLICT.value(),
+                ex.getMessage(),
+                "Duration cannot be changed.",
+                request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    @ExceptionHandler(InvalidCheckInDateException.class)
+    public ResponseEntity<ErrorResponseDto> handleInvalidCheckInDate(InvalidCheckInDateException ex,HttpServletRequest request){
+        ErrorResponseDto response = new ErrorResponseDto(
+                HttpStatus.BAD_REQUEST.value(),
+                "Invalid check-in date.",
+                ex.getMessage(),
+                request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(InvalidCredantialsException.class)
+    public ResponseEntity<ErrorResponseDto> handleInvalidCredentials(InvalidCredantialsException ex,HttpServletRequest request){
+        ErrorResponseDto response = new ErrorResponseDto(
+                HttpStatus.UNAUTHORIZED.value(),
+                "Invalid credentials.",
+                ex.getMessage(),
+                request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    }
+
+    @ExceptionHandler(InvalidDateException.class)
+    public ResponseEntity<ErrorResponseDto> handleInvalidDate(InvalidDateException ex,HttpServletRequest request){
+        ErrorResponseDto response = new ErrorResponseDto(
+                HttpStatus.BAD_REQUEST.value(),
+                "Invalid date provided.",
+                ex.getMessage(),
+                request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(RoomExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleRoomExists(RoomExistsException ex, HttpServletRequest request){
+        ErrorResponseDto response = new ErrorResponseDto(
+                HttpStatus.CONFLICT.value(),
+                "Room already exists.",
+                ex.getMessage(),
+                request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        return  ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    @ExceptionHandler(UserExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleUserExistsException(UserExistsException ex, HttpServletRequest request){
+        ErrorResponseDto response = new ErrorResponseDto(
+                HttpStatus.CONFLICT.value(),
+                "User already exists.",
+                ex.getMessage(),
+                request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        return  ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleUserNotFoundException(UserNotFoundException ex, HttpServletRequest request){
+        ErrorResponseDto errorResponse = new ErrorResponseDto(
+                HttpStatus.NOT_FOUND.value(),
+                "User not found",
+                ex.getMessage(),
+                request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        return   ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(UserNotActiveException.class)
+    public ResponseEntity<ErrorResponseDto> handleUserNotActiveException(UserNotActiveException ex, HttpServletRequest request){
+        ErrorResponseDto response = new ErrorResponseDto(
+                HttpStatus.FORBIDDEN.value(),
+                "User account is not active.",
+                ex.getMessage(),
+                request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        return  ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
     }
 }
