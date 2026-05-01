cfbcfa0d93932251e0343eb3fdaa6f6d67bdd4ff
Mansura <badalovamansura40@gmail.com>
2026-05-02 00:09:33 +0400

    adding exception for clead code

---
+++ src/main/java/org/ironhack/hotelbookingapp/exception/BookingPayedException.java
@@ -0,0 +1,7 @@
+package org.ironhack.hotelbookingapp.exception;
+
+public class BookingPayedException extends RuntimeException {
+    public BookingPayedException(String message) {
+        super(message);
+    }
+}
