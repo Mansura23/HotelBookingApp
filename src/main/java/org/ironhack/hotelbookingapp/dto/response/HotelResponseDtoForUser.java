4f076603e9b1abd09aa2596008ae6e11bb5193dd
Mansura <badalovamansura40@gmail.com>
2026-05-01 13:51:01 +0400

    adding HotelResponseDtoForUser

---
+++ src/main/java/org/ironhack/hotelbookingapp/dto/response/HotelResponseDtoForUser.java
@@ -0,0 +1,23 @@
+package org.ironhack.hotelbookingapp.dto.response;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.ironhack.hotelbookingapp.entity.Room;
+
+import java.util.List;
+
+@Data
+public class HotelResponseDtoForUser {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String country;
+    private String zipCode;
+    private String phone;
+    private String city;
+    private String address;
+    private double rating;
+    private List<Room> rooms;
+
+}
