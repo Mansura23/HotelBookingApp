d4fbefb8c7bac4815a6866f0e1cf2a460e2f9a2b
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-05-03 01:38:03 +0400

    AdminUpdateRequest refactored

--- src/main/java/org/ironhack/hotelbookingapp/dto/request/AdminUpdateRequestDto.java
+++ src/main/java/org/ironhack/hotelbookingapp/dto/request/AdminUpdateRequestDto.java
@@ -8,26 +8,21 @@ import java.math.BigDecimal;
 
 @Data
 public class AdminUpdateRequestDto {
-    @NotBlank(message = "First name cannot be empty")
+    @Size(min = 3, max = 20)
     private String firstName;
 
-    @NotBlank(message = "Last name cannot be empty")
+    @Size(min = 3, max = 20)
     private String lastName;
 
-    @NotBlank(message = "Phone number cannot be empty")
     private String number;
 
-    @NotBlank(message = "Password cannot be empty")
     @Size(min = 6, max = 12, message = "Password must be between 6 and 12 characters")
     private String password;
 
-    @NotNull(message = "Role cannot be null")
     private Role role;
 
-    @NotNull(message = "Status cannot be null")
     private Status status;
 
-    @NotNull(message = "Balance cannot be null")
     @DecimalMin("0.01")
     @Digits(integer = 6, fraction = 2)
     private BigDecimal balance;
