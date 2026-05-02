c0176bd8444b0c4e7d13fbdf13ebc4446e657c74
Mansura <badalovamansura40@gmail.com>
2026-05-02 10:06:58 +0400

    changing @NotBlank to @NotNull

--- src/main/java/org/ironhack/hotelbookingapp/dto/request/PaymentRequestDto.java
+++ src/main/java/org/ironhack/hotelbookingapp/dto/request/PaymentRequestDto.java
@@ -1,6 +1,5 @@
 package org.ironhack.hotelbookingapp.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.ironhack.hotelbookingapp.enums.PaymentMethod;
